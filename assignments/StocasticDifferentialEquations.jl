fd6fc45db52050fca6630d5ea8d7223ba8bed507
youainti <youainti@protonmail.com>
2021-10-08 12:58:00 -0700

    got basic stoch proc in there

--- assignments/StocasticDifferentialEquations.jl
+++ assignments/StocasticDifferentialEquations.jl
@@ -14,14 +14,16 @@ using .StochasticDifferentialEquations
 =#
 
 #=
-Create a struct to capture the type of exit from a 1d box, with a couple of helpful methods
+Create a struct to capture the type of exit, with a couple of helpful methods
 =#
-struct ExitType1d
+abstract type ExitCondition end
+#note the implicit contract below
+measured_time(exit_type::ExitCondition) = exit_type.time
+
+struct Exit1dBox <: ExitCondition
     time::Int
     exited_top::Bool
 end
-
-measured_time(exit_type::ExitType1d) = exit_type.time
 exited_top_boundary(exit_type::ExitType1d) = exit_type.exited_top
 exited_bottom_boundary(exit_type::ExitType1d) = !exit_type.exited_top
 
@@ -31,11 +33,11 @@ exited_bottom_boundary(exit_type::ExitType1d) = !exit_type.exited_top
 The following define a Wiener Process Structure and a set of useful functions 
 for working with Weiner Paths
 =#
-struct WienerPath
+struct WienerPath <: Path
     # the two key things to know are the sampled points from the path and 
     # how far apart they are in the coordinate system
-    path::Vector{Float64}
-    Δt::Float64
+    path::Vector{Float}
+    Δt::Float
    
     #This overrides the default creation function.
     # It creates a wiener process using a given path, while 
@@ -191,19 +193,35 @@ stochastic process (lazy, it represents a generation procedure)
     bound and delta information
     the functions that make it up
 Two important methods are 
-   walk_until(exit_fn) -> ExitCondition(stochastic_path, exit_info): which creates the stochastic integral, walking until an exit condition is met.
-   walk_for(n) -> stochastic_path
+   walk_until(exit_fn) -> ExitCondition(stochastic_path, exit_info): which creates the stochastic path by walking until an exit condition is met.
+   walk_for(n) -> stochastic_path: which creates a stochastic path of length n
+=#
+struct StocasticProcess 
+    Δt::Float
+    functions_list::Array{Function} #TODO: check later
+end
 
+
+function walk_until(sp::StochasticProcess, exit_fn::Function)
+    return nothing #exit condition
+end
+
+function walk_for(sp::StochasticProcess,n::UInt)
+    return nothing
+end
+
+#=
 stochastic path
  it should contain
     wiener_path it is built on
     functions it is build out of
     the array of current values.
  important methods include
-    integration
-    cumulative integration
-    refine_path
-    spread_path
+    integration *
+    cumulative integration *
+    refine_path *
+    spread_path *
+    time_period: the last period available 
 
 There is also the question of whether or not to add a cumulative integration function.
 it would need to add integration to it's functions list.
@@ -211,8 +229,113 @@ it would need to add integration to it's functions list.
 There is still a question of whether or not to build a stochastic path that doesn't have 
 some of the fundamentals such as the base wiener path. 
 This may be useful as a return type for some functions.
-I'm not sure what to call it.
 =#
+struct StochasticPath 
+    wiener_path::WienerPath
+    functions_list::Vector{Function} #TODO: check later
+    #These functions need to take in a path and then return a "path"
+    # f(sp::StochasticPath) -> StochasticPath
+    #See the cumulative_integration function for an example of what the new functions_list needs to look like.
+    path::Vector{Float}
+    
+    #change the creation method to verify the length of the WP and path are the same.
+end
+
+function path_to_process(sp::StochasticPath)
+    return StochasticProcess(sp.wiener_path.Δt,sp.functions_list)
+end
+
+function cumulative_integrate(fn::Function, sp::StochasticPath)
+    #=
+        This function integrates across the time domain, by computing the riemann sum
+        associated with the appropriate ito integral.
+        
+        the function must have the signature fn(X\_t, W\_t,\Delta t, t) -> Float
+        Ideally fn = \mu dt + \sigma dW\_t
+        
+        it returns a Float64 value for the integral
+    =#
+        
+    sum = 0.0
+    time_tracker = 0.0
+    cum_integral = similar(sp.path)
+    
+    steps = get_steps(sp.wiener_path)
+    
+    for i in 1:length(wp.path)-1
+        #Calculate the running sum
+        sum += fn(sp.path[i], sp.wiener_path[i], sp.wiener_path.Δt, time_tracker) 
+        cum_integral[i] = sum
+        #iterate time tracker
+        time_tracker += wp.Δt
+    end
+    
+    return StochasticPath(
+        sp.wiener_path
+        ,append!(sp.functions, x -> cumulative_integrate(fn,x))
+        ,cum_integral
+    )
+end
+function integrate(fn::Function, sp::StochasticPath)
+    #=
+        This function integrates across the time domain, by computing the riemann sum
+        associated with the appropriate ito integral.
+        
+        the function must have the signature fn(X\_t, W\_t,\Delta t, t) -> Float
+        Ideally fn = \mu dt + \sigma dW\_t
+        
+        it returns a Float64 value for the integral
+    =#
+        
+    sum = 0.0
+    time_tracker = 0.0
+    
+    steps = get_steps(sp.wiener_path)
+    
+    for i in 1:length(wp.path)-1
+        #Calculate the running sum
+        sum += fn(sp.path[i], sp.wiener_path[i], sp.wiener_path.Δt, time_tracker) 
+        #iterate time tracker
+        time_tracker += wp.Δt
+    end
+    
+    return sum
+end
+
+function refine_path(sp::StochasticPath)
+    #=
+    refines a stochastic path by refining the WP underling it and recalculating.
+    =#
+    wp = refine_path(sp.wiener_path)
+    path = similar(wp)
+    
+    for fn in sp.functions_list
+        path = fn(path)
+    end
+    
+    return StochasticPath(
+        wp
+        ,sp.functions_list
+        ,path
+    )
+end
+
+function spread_path(sp::StochasticPath)
+    #=
+    =#
+    wp = spread_path(sp.wiener_path)
+    path = similar(wp)
+    
+    for fn in sp.functions_list
+        path = fn(path)
+    end
+    
+    return StochasticPath(
+        wp
+        ,sp.functions_list
+        ,path
+    )
+end
 
 #=
 I also need to create a set of useful integrands.
