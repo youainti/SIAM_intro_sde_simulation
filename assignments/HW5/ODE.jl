a0972a4c8e4851bff862fcb06dc5537a9f9c7edf
youainti <youainti@protonmail.com>
2021-10-18 17:11:42 -0700

    current versions

--- assignments/HW5/ODE.jl
+++ assignments/HW5/ODE.jl
@@ -1,5 +1,5 @@
 module ODE
-export FundamentalNumericalBase, ExtendedNumericalBase, euler_step, euler_method, euler_method_with_local_errors, heuns_step, heuns_method
+export NumericalBase,FundamentalNumericalBase, ExtendedNumericalBase, euler_step, euler_method, euler_method_with_local_errors, heuns_step, heuns_method, heuns_method_with_local_errors,ab_method_with_local_errors,richardson_extrapolation_of_euler
 #implement Euler
 abstract type NumericalBase end
 
@@ -28,7 +28,7 @@ function euler_method(nb::NumericalBase, x0::Float64, time_start::Float64, time_
     t = time_start
     
     for i in 2:iterations
-        println(i," ", iterations)
+        #println(i," ", iterations)
         X[i] = euler_step(nb,X[i-1],t)
         t+= nb.Δt
     end
@@ -64,68 +64,90 @@ function euler_method_with_local_errors(nb::ExtendedNumericalBase, x0::Float64, 
     return X,Y,Z
 end
 
-function make_method(step::Function)
-    function ODE_method(
-            nb::NumericalBase
-            , x0::Float64
-            , time_start::Float64
-            , time_stop::Float64
-        )
-
-        iterations = Int(ceil((time_stop-time_start)/nb.Δt))
-
-        X = zeros(iterations) 
-        X[1] = x0
-        t = time_start
-
-        for i in 2:iterations
-            X[i] = step(nb,X[i-1],t)
-            t+= nb.Δt
-        end
-
-        return X
-    end
-end
 
 
-function make_method_with_local_errors(step::Function)
-    function ODE_method_with_errors(
-        nb::ExtendedNumericalBase
-        , x0::Float64
-        , time_start::Float64
-        , time_stop::Float64
-    )
 
-        iterations = Int(ceil((time_stop-time_start)/nb.Δt))
-
-        X = zeros(iterations) #Incrementally corrected sequence
-        Y = zeros(iterations) #Actual Values
-        Z = zeros(iterations) #Full estimated sequence
-
-        X[1] = x0
-        Y[1] = x0
-        Z[1] = x0
-
-        t = time_start
-
-        for i in 2:iterations
-            X[i] = step(nb,Y[i-1],t)
-            Y[i] = nb.f(t)
-            Z[i] = step(nb,Z[i-1],t)
-            t+= nb.Δt
-        end
-
-        return X,Y,Z
-    end
-    return ODE_method_with_errors
-end
-
-
-function heuns_step(fb::NumericalBase,y0,t)
+function heuns_step(nb::NumericalBase,y0,t)
     return y0 + 0.5 * ( nb.a(y0,t) + nb.a(euler_step(nb,y0,t),t)) * nb.Δt
 end
 
-heuns_method = make_method(heuns_step)
-heuns_method_with_local_errors = make_method_with_local_errors(heuns_step)
+function heuns_method_with_local_errors(nb::ExtendedNumericalBase, x0::Float64, time_start::Float64, time_stop::Float64)
+    
+    iterations = Int(ceil((time_stop-time_start)/nb.Δt))
+    
+    X = zeros(iterations) #Local Errors
+    Y = zeros(iterations) #Actual Values
+    Z = zeros(iterations) #Full estimation
+    
+    
+    X[1] = x0
+    Y[1] = x0
+    Z[1] = x0
+    
+    t = time_start
+    
+    for i in 2:iterations
+        X[i] = heuns_step(nb,Y[i-1],t)
+        Y[i] = nb.f(t)
+        Z[i] = heuns_step(nb,Z[i-1],t)
+        t+= nb.Δt
+    end
+    
+    return X,Y,Z
+end
+
+
+function adam_brashford_step(
+        nb::NumericalBase
+        ,y2::Float64 #value at step 2
+        ,y1::Float64 #value at step 1
+        ,y0::Float64 #value at step 0
+        ,t::Float64 #initial time
+    )
+    t1 = t - nb.Δt
+    t2 = t1 - nb.Δt
+    
+    return y2 + ( 23*nb.a(y2,t) - 16*nb.a(y1,t1) + 5*nb.a(y0,t2))/12
+end
+
+function ab_method_with_local_errors(nb::ExtendedNumericalBase, x0::Float64, time_start::Float64, time_stop::Float64)
+    
+    iterations = Int(ceil((time_stop-time_start)/nb.Δt))
+    
+    X = zeros(iterations) #Local Errors
+    Y = zeros(iterations) #Actual Values
+    Z = zeros(iterations) #Full estimation
+    
+    
+    X[1] = x0
+    Y[1] = x0
+    Z[1] = x0
+    
+    t1 = time_start
+    t2 = t1  + nb.Δt
+    
+    Z[2] = heuns_step(nb,Z[1],t1)
+    Z[3] = heuns_step(nb,Z[2],t2)
+    
+    t = 0.0
+    
+    for i in 4:iterations
+        X[i] = adam_brashford_step(nb,Y[i-1],Y[i-2],Y[i-3],time_start)
+        Y[i] = nb.f(t)
+        Z[i] = adam_brashford_step(nb,Z[i-1],Z[i-2],Z[i-3],time_start)
+        t+= nb.Δt
+    end
+    
+    return X,Y,Z
+end
+
+function richardson_extrapolation_of_euler(nb::NumericalBase, z0, start_time, end_time)
+    nb2 = FundamentalNumericalBase(nb.a,nb.Δt/2)
+    
+    Yn = euler_method(nb,z0, start_time, end_time)
+    Y2n = euler_method(nb2,z0, start_time, end_time)
+    
+    return 2*Y2n - Yn
+end
 
 end #end module
